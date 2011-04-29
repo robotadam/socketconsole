5b8c930fa6fc68ac8ff657344793088ee0ce0df4
Adam Lowry <adam@therobots.org>
2011-04-29 01:33:36 +0000

    Script to find all open sockets and dump their data

---
+++ socketreader.py
@@ -0,0 +1,13 @@
+import glob
+import socket
+
+
+for filename in glob.glob('/tmp/socketdumper-*'):
+    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    s.connect(filename)
+    print "**** %s" % filename
+    buf = s.recv(1024)
+    while buf:
+        print buf
+        buf = s.recv(1024)
+    s.close()
