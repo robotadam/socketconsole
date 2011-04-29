0a2e0798dcd257d1c0f3b9cf923af38487d3adde
Adam Lowry <adam@therobots.org>
2011-04-28 18:53:43 -0700

    Make sure script is included

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 
 # To set __version__
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 setup(name="socketconsole",
     version=__version__,
-    py_modules=['socketconsole'],
+    py_modules=['socketconsole', 'socketreader'],
     description="Unix socket access to python thread dump",
     zip_safe=False,
     entry_points={
